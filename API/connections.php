7af2fe5aff395d2e40c648c6d93ee65d05454aee
joav <jim.acero.valcarcel@gmail.com>
2017-10-12 19:45:14 -0500

    Inicio del proyecto
    Armando el API...

---
+++ API/connections.php
@@ -0,0 +1,19 @@
+<?php header('Content-Type: application/json');
+extract($_GET);
+switch ($type) {
+ 	case 'usuario':
+ 		switch ($action) {
+ 			case 'insert':
+ 				
+ 				break;
+ 			
+ 			default:
+ 				# code...
+ 				break;
+ 		}
+ 		break;
+ 	
+ 	default:
+ 		# code...
+ 		break;
+ } ?>
